7521e5b88330ddbd6ed9699dfe2b8dc6b8e43720
chimizou <hamza.benbouzid@gmail.com>
2020-05-22 18:10:55 +0200

    Fix delete candidate

--- src/app/candidates/candidates.component.ts
+++ src/app/candidates/candidates.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { CandidatesService } from '../services/candidates.service'
 import { Router } from '@angular/router';
 
@@ -16,7 +15,7 @@ export class CandidatesComponent implements OnInit {
   public totalPages: number;
   public pages: Array<number>;
   public currentKeyWord: string;
-  private initialForm: any = {keyWord: ""};
+  private initialForm: any = { keyWord: "" };
 
   constructor(private candidatesService: CandidatesService, private router: Router) { }
 
@@ -26,51 +25,58 @@ export class CandidatesComponent implements OnInit {
 
   onGetCandidates() {
     this.candidatesService.getCandidatesByPage(this.currentPage, this.size)
-    .subscribe(data => {
-      this.totalPages = data["page"].totalPages;    //data.page.totalPages est possible aussi syntaxique, sauf qu'il va lever une erreur de compilation.
-      this.pages = new Array<number>(this.totalPages);
-      this.candidates = data;
-    }, err => {
-      console.log(err)
-    })
+      .subscribe(data => {
+        this.totalPages = data["page"].totalPages;    //data.page.totalPages est possible aussi syntaxique, sauf qu'il va lever une erreur de compilation.
+        this.pages = new Array<number>(this.totalPages);
+        this.candidates = data;
+      }, err => {
+        console.log(err)
+      })
   }
 
-  onPageCandidate(pageNumber: number){
+  onPageCandidate(pageNumber: number) {
     this.currentPage = pageNumber;
     this.searchCandidates();
   }
 
-  onSearchCandidates(formValue : any) {
-    this.currentPage =0;
+  onSearchCandidates(formValue: any) {
+    this.currentPage = 0;
     this.currentKeyWord = formValue.keyWord;
     this.searchCandidates();
   }
 
-  searchCandidates(){
+  searchCandidates() {
     this.candidatesService.searchCandidatesByFirstName(this.currentKeyWord, this.currentPage, this.size)
-        .subscribe(data => {
+      .subscribe(data => {
+        if (data) {
           console.log(data);
           this.totalPages = data["totalPages"];    //data.page.totalPages est possible aussi syntaxique, sauf qu'il va lever une erreur de compilation.
           this.pages = new Array<number>(this.totalPages);
           this.candidates = data["content"];
-        }, err => {
-          console.log(err);
-        })
-  }
-
-  onDeleteCandidate(candidate: any){
-    let choice = confirm("êtes vous sûr de vouloir supprimer ce candidat ?")
-    if (choice) {
-      this.candidatesService.deleteResource(candidate._links.self.href)
-      .subscribe(data => {
-        this.searchCandidates();
+        }
       }, err => {
         console.log(err);
-      });
+      })
+  }
+
+  onDeleteCandidate(candidateId: number) {
+    let choice = confirm("êtes vous sûr de vouloir supprimer ce candidat ?")
+    if (choice) {
+      this.candidatesService.deleteResource("/api/candidate/deleteCandidate/" + candidateId)
+        .subscribe(data => {
+          const index = this.candidates.findIndex(candidate => candidate.idCandidate === candidateId);
+          console.log(index);
+          if (index !== -1) {
+            this.candidates.splice(index, 1);
+          }
+          console.log(this.candidates);
+        }, err => {
+          console.log(err);
+        });
     }
   }
 
-  onEditCandidate(candidateId: number){
+  onEditCandidate(candidateId: number) {
     this.router.navigateByUrl("/edit-candidate/" + candidateId);
   }
 
